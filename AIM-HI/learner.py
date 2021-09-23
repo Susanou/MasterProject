81c15d2f9bee5fade4876199229dba72c09a75a0
Fukurou <caos2804@gmail.com>
2021-09-23 13:43:51 -0400

    start of learner

---
+++ AIM-HI/learner.py
@@ -0,0 +1,41 @@
+from resnet import ResNet18
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+import os
+import numpy as np
+import pickle
+
+##############
+# Global Vars
+##############
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=2)
+    
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+criterion = nn.CrossEntropyLoss()
+
