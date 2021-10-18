6ad6a1a5e632747ef8ca5d3733768e2d44544095
Fukurou <caos2804@gmail.com>
2021-10-18 10:45:49 -0400

    removed start comment

--- AIM-HI/learner.py
+++ AIM-HI/learner.py
@@ -23,38 +23,6 @@ import pickle
 ##############
 # Global Vars
 ##############
-"""
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-criterion = nn.CrossEntropyLoss()
-
-
-
-local_trainset = torch.utils.data.Subset(trainset, list(range(0, 5000)))
-print("local data", len(local_trainset))
-global_trainset = torch.utils.data.Subset(trainset, list(range(5000, 50000)))
-print("global data", len(global_trainset))
-global_loader = torch.utils.data.DataLoader(global_trainset, batch_size=100, shuffle=False, num_workers=2)
-"""
 
 learners = []
 
