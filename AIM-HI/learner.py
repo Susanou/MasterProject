b508ac8e985a0bb535e49c959165dab117120fda
Fukurou <caos2804@gmail.com>
2021-10-24 19:07:54 -0400

    normalization of train set

--- AIM-HI/learner.py
+++ AIM-HI/learner.py
@@ -34,6 +34,8 @@ def create_model():
 learners = []
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+x_train = x_train/255.0
+x_test = x_test/255.0
 
 n_learners = 2 # Change that later
 theta = 4
