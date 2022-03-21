e2bedcdb55d242ad4643db5fb337ecac5061c6f4
Fukurou <caos2804@gmail.com>
2022-03-21 10:24:25 -0400

    ml privacy plots and attack

---
+++ ml_privacy_meter/tutorials/attack_fl.py
@@ -0,0 +1,59 @@
+import numpy as np
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+import ml_privacy_meter
+import tensorflow as tf
+
+# Load saved target model to attack
+cprefix = 'target.tf'
+cmodelA = tf.keras.models.load_model(cprefix)
+cprefix = 'model_0.tf'
+cmodelB = tf.keras.models.load_model(cprefix)
+
+cmodelA.summary()
+cmodelB.summary()
+
+def preprocess_cifar10_dataset():
+    input_shape = (32, 32, 3)
+    num_classes = 100
+
+    # Split the data between train and test sets
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+    return x_train, y_train, x_test, y_test, input_shape, num_classes
+
+
+x_train, y_train, x_test, y_test, input_shape, num_classes = preprocess_cifar10_dataset()
+
+# training data of the target model
+num_datapoints = 5000
+x_target_train, y_target_train = x_train[:num_datapoints], y_train[:num_datapoints]
+
+# population data (training data is a subset of this)
+x_population = np.concatenate((x_train, x_test))
+y_population = np.concatenate((y_train, y_test))
+
+datahandlerA = ml_privacy_meter.utils.attack_data.AttackData(x_population=x_population,
+                                                             y_population=y_population,
+                                                             x_target_train=x_target_train,
+                                                             y_target_train=y_target_train,
+                                                             batch_size=100,
+                                                             attack_percentage=10, input_shape=input_shape,
+                                                             normalization=True)
+
+attackobj = ml_privacy_meter.attack.meminf.initialize(
+    target_train_model=cmodelA,
+    target_attack_model=cmodelB,
+    train_datahandler=datahandlerA,
+    attack_datahandler=datahandlerA,
+    layers_to_exploit=[6, 7],
+    gradients_to_exploit=[4],
+    device=None, epochs=10, model_name='target_vitcim_10e_white')
+
+print("starting attack training")
+attackobj.train_attack()
+
+print("Generating plots")
+attackobj.test_attack()
