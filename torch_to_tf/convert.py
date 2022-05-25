56057758ac071a2751b2d7bfee426711b49623e8
Fukurou <caos2804@gmail.com>
2022-05-25 15:33:28 +0200

    updated convert script

--- torch_to_tf/convert.py
+++ torch_to_tf/convert.py
@@ -26,13 +26,14 @@ class Cifar10PaperNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
-        self.fc1 = nn.Linear(2304, 64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
+        self.fc1 = nn.Linear(1024, 64)
         self.fc2 = nn.Linear(64, 10)
         
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        #x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
@@ -62,4 +63,4 @@ for filename in os.listdir(directory):
 
         tf_ref = pytorch_to_keras(trained_model, input_var, [(3, 32, 32,)], verbose=False, change_ordering=False)
         print(tf_ref.summary())
-        tf_ref.save(f'keras_models\\model_{epoch}.tf')
+        tf_ref.save(f'keras_models/model_{epoch}.tf')
